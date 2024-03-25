f76a0438f5c365e6cab75ad588bfd0ed382a288a
Nomenandrianina <nomenandrianinaantonio@gmail.com>
2024-03-25 14:56:31 +0300

    langue fr et en
    langue fr et en

---
+++ resources/lang/fr/models/api.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+  'singular' => 'Dernier évènement',
+  'plural' => 'Derniers évènements',
+  'fields' => 
+  array (
+    'id' => 'Id',
+    'type' => 'Type',
+    'description' => 'Description',
+    'vehicle' => 'Véhicule',
+    'date' => 'Date et heure',
+    'latitude' => 'Latitude',
+    'longitude' => 'Longitude',
+    'created_at' => 'Créé le',
+    'updated_at' => 'Modifiée le',
+  ),
+);
