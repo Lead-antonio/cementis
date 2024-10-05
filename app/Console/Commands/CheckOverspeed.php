f69b5c8abc9e52de51d58f72be3c6b4943a50e08
jonny <dev2@m-tec.mg>
2024-10-05 11:54:23 +0300

    Configuration de Overspeed
    Configuration de Overspeed

---
+++ app/Console/Commands/CheckOverspeed.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\OverSpeedService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class CheckOverspeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:overspeed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $overspeed = new OverSpeedService();
+                
+        $startDate = Carbon::now()->subMonths(2)->endOfMonth();
+        $endDate = Carbon::now()->startOfMonth();
+
+        // $date = "2024-06-01";
+        // $startDate = Carbon::parse($date)->startOfMonth();
+        // $endDate = Carbon::parse($date)->endOfMonth();
+
+        $overspeed->CheckOverSpeed($startDate,$endDate);
+    }
+}
