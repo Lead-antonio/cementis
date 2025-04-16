c5f1abe107e3e5ee2ec1285d123d8d2f29b7ad83
Lead-antonio <lead.dev@m-tec.mg>
2025-04-16 08:30:20 +0300

    Update best & bad scoring
    Update best & bad scoring

--- resources/views/dashboard/bad_scoring.blade.php
+++ resources/views/dashboard/bad_scoring.blade.php
@@ -6,21 +6,23 @@
     $chauffeur_calendar = getDriverByNumberBadge($item->badge_calendar);
 @endphp
     <div class="card rounded-card">
-        <a class="text-decoration-none text-dark" href="{{ route('driver.detail.scoring', ['chauffeur' => $chauffeur_calendar, 'id_planning'  => $selectedPlanning]) }}">
-            <div class="card-body card-list">
-                <div class="number-circle-worst">{{ $totalItems - $key }}</div>
-                <strong class="text-dark"> {{ $chauffeur_calendar }} : </strong>  
-                <span class="badge rounded-pill
-                    {{ 
-                        (round($item->total_point) == 0) ? 'bg-success' : 
-                        (round($item->total_point) > 2 && round($item->total_point) <= 5 ? 'bg-warning' : 
-                        (round($item->total_point) > 5 && round($item->total_point) <= 10 ? 'bg-orange' : 
-                        (round($item->total_point) > 10 ? 'bg-danger' : ''))) 
-                    }}" 
-                >
-                    {{ $item->total_point }}
-                </span>
-            </div>
-        </a>
+        @if (!empty($item->imei) && !empty($item->badge_calendar))
+            <a class="text-decoration-none text-dark" href="{{ route('driver.detail.scoring', ['imei' => $item->imei, 'badge' => $item->badge_calendar, 'id_planning'  => $selectedPlanning]) }}">
+                <div class="card-body card-list">
+                    <div class="number-circle-worst">{{ $totalItems - $key }}</div>
+                    <strong class="text-dark"> {{ $chauffeur_calendar }} : </strong>  
+                    <span class="badge rounded-pill
+                        {{ 
+                            (round($item->total_point) == 0) ? 'bg-success' : 
+                            (round($item->total_point) > 2 && round($item->total_point) <= 5 ? 'bg-warning' : 
+                            (round($item->total_point) > 5 && round($item->total_point) <= 10 ? 'bg-orange' : 
+                            (round($item->total_point) > 10 ? 'bg-danger' : ''))) 
+                        }}" 
+                    >
+                        {{ $item->total_point }}
+                    </span>
+                </div>
+            </a>
+        @endif
     </div>
 @endforeach
